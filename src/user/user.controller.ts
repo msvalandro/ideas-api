8c645726636c826cfe4c46e94de69603855644dc
msvalandro <msvalandro@gmail.com>
2019-08-29 13:01:09 -0300

    authentication

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,25 +1,35 @@
-import { Controller, Post, Get, Body, UsePipes } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  UsePipes,
+  UseGuards,
+} from '@nestjs/common';
 
 import { UserService } from './user.service';
 import { UserDTO } from './user.dto';
 import { ValidationPipe } from '../shared/validation.pipe';
+import { AuthGuard } from '../shared/auth.guard';
+import { User } from './user.decorator';
 
 @Controller()
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get('api/users')
-  showAllUsers() {
+  @UseGuards(new AuthGuard())
+  showAllUsers(@User() user) {
     return this.userService.showAll();
   }
 
-  @Post('/login')
+  @Post('login')
   @UsePipes(new ValidationPipe())
   login(@Body() data: UserDTO) {
     return this.userService.login(data);
   }
 
-  @Post('/register')
+  @Post('register')
   @UsePipes(new ValidationPipe())
   register(@Body() data: UserDTO) {
     return this.userService.register(data);
