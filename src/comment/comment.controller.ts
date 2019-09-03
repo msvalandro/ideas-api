1a59ff526e02a90a80527b4e03ef8a2c2505ba45
msvalandro <msvalandro@gmail.com>
2019-09-03 10:41:58 -0300

    added comment module

---
+++ src/comment/comment.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Param,
+  UseGuards,
+  UsePipes,
+  Post,
+  Body,
+  Delete,
+} from '@nestjs/common';
+
+import { CommentService } from './comment.service';
+import { AuthGuard } from '../shared/auth.guard';
+import { ValidationPipe } from '../shared/validation.pipe';
+import { User } from '../user/user.decorator';
+import { CommentDTO } from './comment.dto';
+
+@Controller('api/comments')
+export class CommentController {
+  constructor(private commentService: CommentService) {}
+
+  @Get('idea/:id')
+  showCommentsByIdea(@Param('id') idea: string) {
+    return this.commentService.showByIdea(idea);
+  }
+
+  @Get('user/:id')
+  showCommentsByUser(@Param('id') user: string) {
+    return this.commentService.showByUser(user);
+  }
+
+  @Post('idea/:id')
+  @UseGuards(new AuthGuard())
+  @UsePipes(new ValidationPipe())
+  createComment(
+    @Param('id') idea: string,
+    @User('id') user: string,
+    @Body() data: CommentDTO,
+  ) {
+    return this.commentService.create(idea, user, data);
+  }
+
+  @Get(':id')
+  showComments(@Param('id') id: string) {
+    return this.commentService.show(id);
+  }
+
+  @Delete(':id')
+  @UseGuards(new AuthGuard())
+  destroyComment(@Param('id') id: string, @User('id') user: string) {
+    return this.commentService.destroy(id, user);
+  }
+}
--- src/idea/idea.entity.ts
+++ src/idea/idea.entity.ts
@@ -6,9 +6,11 @@ import {
   ManyToOne,
   ManyToMany,
   JoinTable,
+  OneToMany,
 } from 'typeorm';
 
 import { UserEntity } from '../user/user.entity';
+import { CommentEntity } from '../comment/comment.entity';
 
 @Entity('idea')
 export class IdeaEntity {
@@ -32,4 +34,7 @@ export class IdeaEntity {
   @ManyToMany(type => UserEntity, { cascade: true })
   @JoinTable()
   downvotes: UserEntity[];
+
+  @OneToMany(type => CommentEntity, comment => comment.idea, { cascade: true })
+  comments: CommentEntity[];
 }
