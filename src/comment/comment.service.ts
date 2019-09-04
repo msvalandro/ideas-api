58101ba63b6a5d221395671591bb8891cbc9df49
msvalandro <msvalandro@gmail.com>
2019-09-04 13:11:50 -0300

    adding pagination

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -26,19 +26,23 @@ export class CommentService {
     return responseObject;
   }
 
-  async showByIdea(id: string) {
-    const idea = await this.ideaRepository.findOne({
-      where: { id },
-      relations: ['comments', 'comments.author', 'comments.idea'],
+  async showByIdea(id: string, page: number = 1) {
+    const comments = await this.commentRepository.find({
+      where: { idea: { id } },
+      relations: ['author'],
+      take: 10,
+      skip: 10 * (page - 1),
     });
 
-    return idea.comments.map(comment => this.toResponseObject(comment));
+    return comments.map(comment => this.toResponseObject(comment));
   }
 
-  async showByUser(id: string) {
+  async showByUser(id: string, page: number = 1) {
     const comments = await this.commentRepository.find({
       where: { author: { id } },
       relations: ['author'],
+      take: 10,
+      skip: 10 * (page - 1),
     });
 
     return comments.map(comment => this.toResponseObject(comment));
